e184eb9b56cf319d4f1e668419f5eab6e4da12df
Benio-sama <vago.kata.viki@gmail.com>
2024-01-31 13:16:03 +0100

    masodik feladat kesz, listahoz adas mukodik, bekeres is, validalas van

--- Doga/Doga/Program.cs
+++ Doga/Doga/Program.cs
@@ -10,6 +10,37 @@ namespace Doga
     {
         static void Main(string[] args)
         {
+            Feladat2();
+            Console.ReadKey();
+        }
+
+        static void Feladat2()
+        {
+            List<int> szamok = new List<int>();
+            Console.WriteLine("kerem adjon meg egy szamot: ");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                if (szam != 0)
+                {
+                    szamok.Add(szam);
+                    while (szam > 0)
+                    {
+                        Console.WriteLine("kerek megegy szamot: ");
+                        szam = Convert.ToInt32(Console.ReadLine());
+                        szamok.Add(szam);
+                    }
+                    Console.WriteLine("kilepett");
+                }
+                else 
+                {
+                    Console.WriteLine("kilepett");
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("az adott erteknek szamnak kell lennie");
+            }
         }
     }
 }
