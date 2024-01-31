79e0dbf7aa9c5378128cdb154eb6c0ff61024e3f
Benio-sama <vago.kata.viki@gmail.com>
2024-01-31 13:25:42 +0100

    3. feladat kesz es mukodik, uj feladatok osztaly letrehozva es metodusok atvitele oda

---
+++ Doga/Doga/Feladatok.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doga
+{
+    internal class Feladatok
+    {
+        private List<int> szamok = new List<int>();
+        public List<int> Szamok { get => szamok; set => szamok = value; }
+
+        public void Feladat2()
+        {
+            Console.WriteLine("kerem adjon meg egy szamot: ");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                if (szam != 0)
+                {
+                    szamok.Add(szam);
+                    Feladat3();
+                    while (szam > 0)
+                    {
+                        Console.WriteLine("kerek megegy szamot: ");
+                        szam = Convert.ToInt32(Console.ReadLine());
+                        szamok.Add(szam);
+                        Feladat3();
+                    }
+                    Console.WriteLine("kilepett");
+                }
+                else
+                {
+                    Console.WriteLine("kilepett");
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("az adott erteknek szamnak kell lennie");
+            }
+        }
+
+        public void Feladat3()
+        {
+            foreach (var item in szamok)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Doga/Doga/Program.cs
+++ Doga/Doga/Program.cs
@@ -10,37 +10,9 @@ namespace Doga
     {
         static void Main(string[] args)
         {
-            Feladat2();
+            Feladatok f = new Feladatok();
+            f.Feladat2();
             Console.ReadKey();
-        }
-
-        static void Feladat2()
-        {
-            List<int> szamok = new List<int>();
-            Console.WriteLine("kerem adjon meg egy szamot: ");
-            try
-            {
-                int szam = Convert.ToInt32(Console.ReadLine());
-                if (szam != 0)
-                {
-                    szamok.Add(szam);
-                    while (szam > 0)
-                    {
-                        Console.WriteLine("kerek megegy szamot: ");
-                        szam = Convert.ToInt32(Console.ReadLine());
-                        szamok.Add(szam);
-                    }
-                    Console.WriteLine("kilepett");
-                }
-                else 
-                {
-                    Console.WriteLine("kilepett");
-                }
-            }
-            catch (Exception)
-            {
-                throw new Exception("az adott erteknek szamnak kell lennie");
-            }
         }
     }
 }
